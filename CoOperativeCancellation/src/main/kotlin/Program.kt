0b5fee7c15163f90cae9736239c61163a3a4bc94
Hans Løken <hans@ebamse.net>
2020-04-21 13:45:27 +0200

    Sample code for three different type of co-operative cancellable coroutines

--- CoOperativeCancellation/src/main/kotlin/Program.kt
+++ CoOperativeCancellation/src/main/kotlin/Program.kt
@@ -1,20 +1,36 @@
 import kotlinx.coroutines.*
+import kotlin.coroutines.coroutineContext
 
 fun main(args: Array<String>) = runBlocking {
 
     val job = launch {
         repeat(1000) {
+
+            // Variant #1 using blocking Thread.sleep
 //            print(".")
-//            yield()
-//            Thread.sleep(1)
-            if (coroutineContext.isActive) {
-                print(".")
-                Thread.sleep(10)
-            }
+//            yield() // handles cancellation
+//            Thread.sleep(1) // does not handle cancellation
+//            if (!isActive) throw CancellationException()
+
+            // Variant #2 using delay that implicitly checks for cancellation
+//            print(".")
+//            delay(1)
+
+            // Variant #3 using a cancellable suspend function
+            doPrint()
         }
     }
 
     delay(100)
     job.cancelAndJoin()
     println("done")
+}
+
+suspend fun doPrint() {
+    repeat(1000) {
+        if (!coroutineContext.isActive) throw CancellationException()
+
+        print(".")
+        delay(1)
+    }
 }
